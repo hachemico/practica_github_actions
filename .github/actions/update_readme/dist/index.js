03e9ee76876331310972100002c455bfadc6aa30
root <hachemico@gmail.com>
2022-02-18 13:42:52 +0100

    modificar  actions core

--- .github/actions/update_readme/dist/index.js
+++ .github/actions/update_readme/dist/index.js
@@ -1,20 +1,20 @@
-const core = require('@actions/core');
-const fs = require('fs');
+import { getInput } from '@actions/core';
+import { readFile, writeFile } from 'fs';
 
-let resultado = core.getInput('cypressTest');
+let resultado = getInput('cypressTest');
 let badge;
 
 resultado == 'success'
                 ? badge = '![Success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)'
                 : badge = '![Failure](https://img.shields.io/badge/test-failure-red)'
 
-fs.readFile('README.md', 'utf8', function(err, data) {
+readFile('README.md', 'utf8', function(err, data) {
     if (err) {
         return console.log(err);
     }
     var result = data.replace(/(?<=\<!---Start place for the badge --\>\n)[^]+(?=\n\<!---End place for the badge  --\>)/g, badge);
 
-    fs.writeFile('README.md', result, 'utf8', function(err) {
+    writeFile('README.md', result, 'utf8', function(err) {
         if (err) return console.log(err);
     });
 });
